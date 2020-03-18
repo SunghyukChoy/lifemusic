04c665d6de3c256de63e45cace5a37cfa5b08b5c
Sunghyuk Choi <likeaglow@gmail.com>
2020-03-19 00:04:41 +0900

    first commit

---
+++ src/main/java/my/sunghyuk/lifemusic/HomeController.java
@@ -0,0 +1,54 @@
+package my.sunghyuk.lifemusic;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import my.sunghyuk.lifemusic.dao.BbsDAO;
+import my.sunghyuk.lifemusic.model.Bbs;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/board", method = RequestMethod.GET)
+	public String board(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		BbsDAO dao = new BbsDAO();
+		ArrayList<Bbs> bbs = dao.getList(1);
+		
+		model.addAttribute("bbs", bbs);
+		
+		return "board";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/board.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://www.springframework.org/tags" prefix="sp" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<table>
+	<thead>
+		<tr>
+			<th></th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+</body>
+</html>
