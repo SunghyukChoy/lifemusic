8fd733a4dadca69578407f070dbc5fb03ddb3bf2
"Sunghyuk Choi" <likeaglow@gmail.com>
2020-03-24 17:55:17 +0900

    commit test again

--- src/test/resources/my/sunghyuk/lifemusic/test/CommitTest.java
+++ src/test/resources/my/sunghyuk/lifemusic/test/CommitTest.java
@@ -4,6 +4,7 @@ public class CommitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("CommitTest");
+		System.out.println("CommitTest");
 
 	}
 
